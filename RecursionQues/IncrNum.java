5660c0cc587b7ea71f63917d606c174d52aa23bb
AmanSah078 <amanalisiwan22@gmail.com>
2025-09-13 08:44:56 +0530

    IncrNum.java

--- RecursionQues/IncrNum.java
+++ RecursionQues/IncrNum.java
@@ -1,1 +1,48 @@
 
+class IncrNum{
+	public static int m1(int n)
+	{
+		if(n==0)
+		{
+			return;
+		}
+		//1st excepted output
+		m1(n-1);
+		//2nd i want to do my work
+		System.out.println(n);
+	}
+	public static void main(String[] args)
+	{
+		m1(5);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
