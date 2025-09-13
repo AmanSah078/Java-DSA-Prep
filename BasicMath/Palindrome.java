20add9e693b96298c53bd5a0e7a3406562cd8452
AmanSah078 <amanalisiwan22@gmail.com>
2025-09-13 08:39:05 +0530

    Palindrome.java

--- BasicMath/Palindrome.java
+++ BasicMath/Palindrome.java
@@ -1,1 +1,25 @@
+import java.util.*;
+class Palindrome {
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int rev=0;
+		int original=n;
+		while(n!=0)
+		{
+			int digits=n%10;
+		 rev=rev*10+digits;
+			n=n/10;
+
+		}
+			if(rev==original)
+			{
+				System.out.println("it is palindrome number");
+			}
+			else {
+				System.out.println("Not Palindrome");
+			}
+		}
+	}
 
