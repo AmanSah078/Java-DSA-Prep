87a4608eb2daf4a028944d347f6a8f8fbc973937
Aman Sah <amanalisiwan22@gmail.com>
2025-09-14 08:47:22 +0530

    DecrNum.java

--- RecursionQues/DecrNum.java
+++ RecursionQues/DecrNum.java
@@ -1,1 +1,21 @@
-
+import java.util.*;
+class DecrNum {
+	public static void m1(int n)
+	{
+		//Base Case
+		if(n==0)
+		{
+			return;
+		}
+ //First I should Perform The operation   
+		System.out.println(n);
+ //Then u Dude   
+		m1(n-1);
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		m1(n);
+	}
+}
