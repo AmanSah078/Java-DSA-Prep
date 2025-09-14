fa2fffbffad468f7edc8b36ba5fbac5cb005413a
Aman Sah <amanalisiwan22@gmail.com>
2025-09-14 10:52:36 +0530

    Max.java

---
+++ RecursionQues/Max.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Test {
+	public static int m1(int[] arr, int idx)
+	{
+//Because we can only compare to the last index na 
+		//we cannot to the compare to length of the Array		
+		if(idx==arr.length-1)
+		{
+			return arr[idx];
+		}
+		int misa=m1(arr,idx+1);
+		{
+			if(misa>arr[idx])
+			{
+				return misa;
+			}
+			else {
+				return arr[idx];
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr= new int[n];
+		for(int i=0; i<arr.length; i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+	int result=m1(arr,0);
+		System.out.println(result);
+	}
+}
