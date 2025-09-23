906e0c7d494c051c12f9c2b2d1118634c5648e2d
Aman Sah <amanalisiwan22@gmail.com>
2025-09-23 10:17:31 +0530

    IspowerOfTwoornot.java

--- BitManupulation/IspowerOfTwoornot.java
+++ BitManupulation/IspowerOfTwoornot.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class IspowerOfTwoornot {
+  public static boolean ispower(int n)
+  {
+    return(n&(n-1))==0;
+  }
+  public static void main(String[] args)
+  {
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+    int result=ispower(n);
+    System.out.println(result);
+}
+}
