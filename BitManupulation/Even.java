b547a5df9691a8583ddf2a592d19b7562bd85e1f
Aman Sah <amanalisiwan22@gmail.com>
2025-09-23 10:05:54 +0530

    Even.java

--- BitManupulation/Even.java
+++ BitManupulation/Even.java
@@ -1,1 +1,16 @@
 
+import java.util.*;
+class Even {
+  public static void main(String[] args)
+  {
+  //Bhai if n=5 when u find the binary number of this number then your LSB=1 then u will get odd
+  // But if n=4 then again when u find the Binary of this number then u get LSB=0
+	  int bitmask=1;
+	  if(n & bitmask==0)
+	  {
+		  System.out.println("even number");
+	  }
+	  else {
+		  System.out.println("odd number");
+	  }
+  }
