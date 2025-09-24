24e86588a5388629153cb8bd3d32cb68b7c2cf37
Aman Sah <amanalisiwan22@gmail.com>
2025-09-24 09:53:33 +0530

    SearchElement.java

--- 2D_Array/SearchElement.java
+++ 2D_Array/SearchElement.java
@@ -1,1 +1,40 @@
+import java.util.*;
+class Search {
+	public static boolean search(int matrix[][], int key)
+	{
+		int n=matrix.length;
+		int m=matrix[0].length;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<m; j++)
+			{
+				if(matrix[i][j]==key)
+				{
+					System.out.println("key found at " + i + " , " + j);
+					return true;
+				}
+			}
+		}
+		System.out.println("Sorry Key is not found");
+		return false;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		
+		int matrix[][]=new int[3][3];
+		int n=matrix.length;
+		int m=matrix[0].length;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<m; j++)
+			{
+				matrix[i][j]=sc.nextInt();
+			}
+		}
+
+		
+	search(matrix,9);
+	}
+}
 
